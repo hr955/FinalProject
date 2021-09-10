83e77ef210b7bc18947dcbcbf71324fb5be881fd
hr955 <nfzoze01@gmail.com>
2021-09-10 12:15:06 +0900

    feat #1 로그인 UI 생성

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -2,15 +2,15 @@ package com.example.finalproject
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import com.example.finalproject.databinding.ActivityMainBinding
+import com.example.finalproject.databinding.ActivityLoginBinding
 
 class MainActivity : BaseActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
         setupEvents()
         setValues()
