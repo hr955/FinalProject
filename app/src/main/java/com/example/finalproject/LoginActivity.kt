70a7b12787233c659b8cb5b88377e87d5a955c57
hr955 <nfzoze01@gmail.com>
2021-09-10 12:44:06 +0900

    feat #1 페이스북 로그인시 사용자 정보 받아오기

--- app/src/main/java/com/example/finalproject/LoginActivity.kt
+++ app/src/main/java/com/example/finalproject/LoginActivity.kt
@@ -3,14 +3,13 @@ package com.example.finalproject
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.finalproject.databinding.ActivityLoginBinding
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
 import com.facebook.login.LoginResult
 import android.content.Intent
+import android.util.Log
+import com.facebook.*
 import com.facebook.login.LoginManager
+import org.json.JSONObject
 import java.util.*
-
 
 class LoginActivity : BaseActivity() {
 
@@ -26,7 +25,6 @@ class LoginActivity : BaseActivity() {
 
     }
 
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         callbackManager.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
@@ -36,27 +34,41 @@ class LoginActivity : BaseActivity() {
         callbackManager = CallbackManager.Factory.create()
 
         binding.btnFacebookLogin.setOnClickListener {
-            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("email"))
+            val loginManager = LoginManager.getInstance()
+
+            loginManager.logInWithReadPermissions(this, Arrays.asList("public_profile"))
+
+            loginManager.registerCallback(
+                callbackManager,
+                object : FacebookCallback<LoginResult?> {
+                    override fun onSuccess(result: LoginResult?) {
+                        // 로그인 성공시 -> 페이스북에 접근할 수 있는 토큰 발급
+                        // 유저 정보 받아오기
+                        val graphRequest = GraphRequest.newMeRequest(result?.accessToken,
+                            object : GraphRequest.GraphJSONObjectCallback {
+                                override fun onCompleted(
+                                    obj: JSONObject?,
+                                    response: GraphResponse?
+                                ) {
+                                    val name = obj!!.getString("name")
+                                    val id = obj!!.getString("id")
+
+                                    Log.d("이름", name)
+                                    Log.d("user_id", id)
+
+                                }
+                            })
+                        graphRequest.executeAsync()
+                    }
+
+                    override fun onCancel() {
+                    }
+
+                    override fun onError(error: FacebookException?) {
+                    }
+                }
+            )
         }
-
-        binding.loginButton.setReadPermissions("email")
-
-        // Callback registration
-        binding.loginButton.registerCallback(
-            callbackManager,
-            object : FacebookCallback<LoginResult?> {
-                override fun onSuccess(loginResult: LoginResult?) {
-                    // App code
-                }
-
-                override fun onCancel() {
-                    // App code
-                }
-
-                override fun onError(exception: FacebookException) {
-                    // App code
-                }
-            })
     }
 
     override fun setValues() {
