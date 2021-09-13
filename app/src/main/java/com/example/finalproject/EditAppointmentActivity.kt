bd44f701b0710905456372fb750db16cf0b005e4
hr955 <nfzoze01@gmail.com>
2021-09-13 12:43:30 +0900

    feat #2 일정을 서버에 저장하는 API 추가

--- app/src/main/java/com/example/finalproject/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/finalproject/EditAppointmentActivity.kt
@@ -2,15 +2,21 @@ package com.example.finalproject
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.finalproject.databinding.ActivityEditAppointmentBinding
+import com.example.finalproject.datas.BasicResponse
+import com.example.finalproject.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.text.SimpleDateFormat
 import java.util.*
 
 class EditAppointmentActivity : BaseActivity() {
 
-    lateinit var binding:ActivityEditAppointmentBinding
+    lateinit var binding: ActivityEditAppointmentBinding
     val mSelectedDateTime = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,29 +29,70 @@ class EditAppointmentActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-        var dateString = ""
-        var timeString = ""
-
         // 날짜 선택 버튼
         binding.btnSelectDate.setOnClickListener {
-            val dateSetListener = DatePickerDialog.OnDateSetListener { datePicker, year, month, dayOfMonth ->
-                dateString = "${year}년 ${month}월 ${dayOfMonth}일"
+            val dateSetListener =
+                DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
+                    mSelectedDateTime.set(year, month, day)
 
-                val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hourOfDay, minute ->
-                    timeString = "${hourOfDay}시 ${minute}분"
-                    binding.txtDate.text = "${dateString} ${timeString}"
+                    val timeSetListener =
+                        TimePickerDialog.OnTimeSetListener { timePicker, hourOfDay, minute ->
+                            mSelectedDateTime.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                            mSelectedDateTime.set(Calendar.MINUTE, minute)
+
+                            binding.txtDate.text =
+                                SimpleDateFormat("yyyy. M. d (E) HH:mm").format(mSelectedDateTime.time)
+                        }
+                    TimePickerDialog(
+                        this,
+                        timeSetListener,
+                        mSelectedDateTime.get(Calendar.HOUR_OF_DAY),
+                        mSelectedDateTime.get(Calendar.MINUTE),
+                        true
+                    ).show()
                 }
-                TimePickerDialog(this, timeSetListener, mSelectedDateTime.get(Calendar.HOUR_OF_DAY), mSelectedDateTime.get(Calendar.MINUTE),true).show()
-            }
-
-            DatePickerDialog(this, dateSetListener, mSelectedDateTime.get(Calendar.YEAR),mSelectedDateTime.get(Calendar.MONTH),mSelectedDateTime.get(Calendar.DAY_OF_MONTH)).show()
+            DatePickerDialog(
+                this,
+                dateSetListener,
+                mSelectedDateTime.get(Calendar.YEAR),
+                mSelectedDateTime.get(Calendar.MONTH),
+                mSelectedDateTime.get(Calendar.DAY_OF_MONTH)
+            ).show()
         }
 
         // 완료 버튼 (일정 저장)
         binding.btnSave.setOnClickListener {
             val inputTitle = binding.edtAppointmentTitle.text.toString()
             val inputPlace = binding.edtPlace.text.toString()
+            val inputDate = SimpleDateFormat("yyyy-MM-dd HH:mm").format(mSelectedDateTime.time)
+            val lat = 37.497919
+            val lon = 127.027469
 
+            apiService.postRequestAppointment(
+                ContextUtil.getToken(mContext),
+                inputTitle,
+                inputDate,
+                inputPlace,
+                lat,
+                lon
+            ).enqueue(object : Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+                    Log.d("테스트", response.body().toString())
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+                    Log.d("테스트", t.message.toString())
+
+                }
+            })
+
+            if (binding.txtDate.text == "") {
+                //날짜를 선택해주세요
+            }
         }
     }
 
