00ccbc5a80de56da78339b8579e49bc916cf8240
hr955 <nfzoze01@gmail.com>
2021-09-13 13:06:55 +0900

    feat #2 API 요청 헤더에 토큰을 담아주는 Interceptor 추가

--- app/src/main/java/com/example/finalproject/web/ServerAPI.kt
+++ app/src/main/java/com/example/finalproject/web/ServerAPI.kt
@@ -1,5 +1,9 @@
 package com.example.finalproject.web
 
+import android.content.Context
+import com.example.finalproject.utils.ContextUtil
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,10 +13,25 @@ class ServerAPI {
 
         private var retrofit: Retrofit? = null
 
-        fun getRetrofit() : Retrofit {
-            if (retrofit == null){
+        fun getRetrofit(context: Context): Retrofit {
+
+            val interceptor = Interceptor {
+                with(it) {
+                    val newRequest =
+                        request().newBuilder()
+                            .addHeader("X-Http-Token", ContextUtil.getToken(context))
+                            .build()
+
+                    proceed(newRequest)
+                }
+            }
+
+            val myClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+            if (retrofit == null) {
                 retrofit = Retrofit.Builder()
                     .baseUrl(HOST_URL)
+                    .client(myClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
             }
