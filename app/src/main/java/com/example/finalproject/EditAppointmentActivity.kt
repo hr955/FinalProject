cad436b7e5d96f77f822f41a17d90c22f65ba44a
hr955 <nfzoze01@gmail.com>
2021-09-13 11:43:45 +0900

    feat #2 날짜 및 시간선택 기능

--- app/src/main/java/com/example/finalproject/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/finalproject/EditAppointmentActivity.kt
@@ -1,11 +1,54 @@
 package com.example.finalproject
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.finalproject.databinding.ActivityEditAppointmentBinding
+import java.util.*
 
-class EditAppointmentActivity : AppCompatActivity() {
+class EditAppointmentActivity : BaseActivity() {
+
+    lateinit var binding:ActivityEditAppointmentBinding
+    val mSelectedDateTime = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_appointment)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_appointment)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+        var dateString = ""
+        var timeString = ""
+
+        // 날짜 선택 버튼
+        binding.btnSelectDate.setOnClickListener {
+            val dateSetListener = DatePickerDialog.OnDateSetListener { datePicker, year, month, dayOfMonth ->
+                dateString = "${year}년 ${month}월 ${dayOfMonth}일"
+
+                val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hourOfDay, minute ->
+                    timeString = "${hourOfDay}시 ${minute}분"
+                    binding.txtDate.text = "${dateString} ${timeString}"
+                }
+                TimePickerDialog(this, timeSetListener, mSelectedDateTime.get(Calendar.HOUR_OF_DAY), mSelectedDateTime.get(Calendar.MINUTE),true).show()
+            }
+
+            DatePickerDialog(this, dateSetListener, mSelectedDateTime.get(Calendar.YEAR),mSelectedDateTime.get(Calendar.MONTH),mSelectedDateTime.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+        // 완료 버튼 (일정 저장)
+        binding.btnSave.setOnClickListener {
+            val inputTitle = binding.edtAppointmentTitle.text.toString()
+            val inputPlace = binding.edtPlace.text.toString()
+
+        }
+    }
+
+    override fun setValues() {
     }
 }
--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.finalproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.database.DatabaseUtils
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.finalproject.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
+
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         setupEvents()
         setValues()
     }
 
     override fun setupEvents() {
+        binding.btnAddAppointment.setOnClickListener {
+            startActivity(Intent(mContext, EditAppointmentActivity::class.java))
+        }
     }
 
     override fun setValues() {
