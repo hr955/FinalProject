cdd382b40896c3e2ad16dfd140051b0398b5479b
hr955 <nfzoze01@gmail.com>
2021-09-14 11:48:25 +0900

    feat #6 정보창 관련 이벤트 - 정보창 Toggle

--- app/src/main/java/com/example/finalproject/ViewMapActivity.kt
+++ app/src/main/java/com/example/finalproject/ViewMapActivity.kt
@@ -2,6 +2,8 @@ package com.example.finalproject
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.finalproject.databinding.ActivityViewMapBinding
 import com.example.finalproject.datas.AppointmentData
@@ -45,21 +47,40 @@ class ViewMapActivity : BaseActivity() {
                 .animate(CameraAnimation.Easing)
             it.moveCamera(cameraUpdate)
 
-            val selectedPointMarker = Marker(coord)
-            selectedPointMarker.icon = OverlayImage.fromResource(R.drawable.ic_pink_marker)
-            selectedPointMarker.map = it
+            val marker = Marker(coord)
+            marker.icon = OverlayImage.fromResource(R.drawable.ic_pink_marker)
+            marker.map = it
 
             val infoWindow = InfoWindow()
-            infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(mContext) {
-                override fun getText(infoWindow: InfoWindow): CharSequence {
+            infoWindow.adapter = object : InfoWindow.DefaultViewAdapter(mContext) {
+                override fun getContentView(infoWindow: InfoWindow): View {
 
-                    val myView = LayoutInflater.from(mContext).inflate(R.layout.my_custom_info_window, null)
+                    val myView =
+                        LayoutInflater.from(mContext).inflate(R.layout.my_custom_info_window, null)
+                    val placeNameTxt = myView.findViewById<TextView>(R.id.txt_place)
+                    val arrivalTimeTxt = myView.findViewById<TextView>(R.id.txt_arrival_time)
 
-                    return mAppointmentData.place
+                    placeNameTxt.text = mAppointmentData.place
+                    arrivalTimeTxt.text = "4시간 예상"
+
+                    return myView
                 }
             }
-            infoWindow.open(selectedPointMarker)
+            infoWindow.open(marker)
 
+            it.setOnMapClickListener { pointF, latLng ->
+                infoWindow.close()
+            }
+
+            marker.setOnClickListener {
+                val clickedMarker = it as Marker
+                if(clickedMarker.infoWindow == null){
+                    infoWindow.open(clickedMarker)
+                }else{
+                    infoWindow.close()
+                }
+                return@setOnClickListener true
+            }
         }
     }
 }
