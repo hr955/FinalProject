127a0364a288a1d0a7a510494a5e0e45acf42c57
hr955 <nfzoze01@gmail.com>
2021-11-07 18:53:02 +0900

    #2 도착지 검색 - 키보드 검색버튼 이벤트 추가

--- app/src/main/java/com/neppplus/gabozago/SetDestinationActivity.kt
+++ app/src/main/java/com/neppplus/gabozago/SetDestinationActivity.kt
@@ -1,6 +1,9 @@
 package com.neppplus.gabozago
 
+import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.neppplus.gabozago.adapters.SetPlaceSearchListAdapter
@@ -26,7 +29,44 @@ class SetDestinationActivity : BaseActivity() {
     override fun setupEvents() {
         binding.btnClose.setOnClickListener { finish() }
 
-        // 도착지 검색
+        // 도착지 검색 (검색 버튼)
+        binding.ivSearchDestination.setOnClickListener {
+            setSearchListAdapter()
+        }
+
+        // 도착지 검색 (키보드 검색 버튼)
+        keyboardSearchBtn()
+
+    }
+
+    override fun setValues() {
+
+    }
+
+    // 키보드 검색버튼
+    private fun keyboardSearchBtn() {
+        binding.edtSetDestination.setOnEditorActionListener { v, actionId, event ->
+            var handled = false
+
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                val inputMethodManager =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(
+                    binding.edtSetDestination.windowToken,
+                    0
+                )
+
+                setSearchListAdapter()
+
+                handled = true
+            }
+
+            handled
+        }
+    }
+
+    // 어댑터 연결
+    private fun setSearchListAdapter() {
         getPlaceSearchList { response ->
             binding.rvDestinationSearchList.apply {
                 adapter = SetPlaceSearchListAdapter(mContext, response)
@@ -35,29 +75,25 @@ class SetDestinationActivity : BaseActivity() {
         }
     }
 
-    override fun setValues() {
-    }
-
-    // 도착지 검색
+    // 도착지 검색 API 호출
     private fun getPlaceSearchList(success: (response: SearchPlaceData) -> Unit) {
-        binding.ivSearchDestination.setOnClickListener {
-            val inputPlaceName = binding.edtSetDestination.text.toString()
+        val inputPlaceName = binding.edtSetDestination.text.toString()
 
-            val service = KakaoAPI.getRetrofit()
-                .getRequestSearchPlace(getString(R.string.kakao_rest_api_key), inputPlaceName)
+        val service = KakaoAPI.getRetrofit()
+            .getRequestSearchPlace(getString(R.string.kakao_rest_api_key), inputPlaceName)
 
-            service.enqueue(object : Callback<SearchPlaceData> {
-                override fun onResponse(
-                    call: Call<SearchPlaceData>,
-                    response: Response<SearchPlaceData>
-                ) {
-                    if (response.isSuccessful) {
-                        success(response.body()!!)
-                    }
+        service.enqueue(object : Callback<SearchPlaceData> {
+            override fun onResponse(
+                call: Call<SearchPlaceData>,
+                response: Response<SearchPlaceData>
+            ) {
+                if (response.isSuccessful) {
+                    success(response.body()!!)
                 }
+            }
 
-                override fun onFailure(call: Call<SearchPlaceData>, t: Throwable) {}
-            })
-        }
+            override fun onFailure(call: Call<SearchPlaceData>, t: Throwable) {}
+        })
+
     }
 }
