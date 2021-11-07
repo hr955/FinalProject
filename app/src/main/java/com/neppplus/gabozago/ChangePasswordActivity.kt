a90fe4c6f8e7d10aecfe4f8ab642be7b60b1d6b8
hr955 <nfzoze01@gmail.com>
2021-11-07 19:08:14 +0900

    #23 툴바 추가

--- app/src/main/java/com/neppplus/gabozago/ChangePasswordActivity.kt
+++ app/src/main/java/com/neppplus/gabozago/ChangePasswordActivity.kt
@@ -25,6 +25,15 @@ class ChangePasswordActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        changePWBtnClickEvent() // 변경 완료 버튼
+        binding.btnClose.setOnClickListener { finish() }
+    }
+
+    override fun setValues() {
+
+    }
+
+    private fun changePWBtnClickEvent() {
         binding.btnChangePassword.setOnClickListener {
             val currentPW = binding.edtCurrentPassword.text.toString()
             val newPW = binding.edtNewPassword.text.toString()
@@ -51,21 +60,18 @@ class ChangePasswordActivity : BaseActivity() {
                         call: Call<BasicResponse>,
                         response: Response<BasicResponse>
                     ) {
-                        if(response.isSuccessful){
+                        if (response.isSuccessful) {
                             Toast.makeText(mContext, "비밀번호가 변경되었습니다", Toast.LENGTH_SHORT).show()
                             ContextUtil.setToken(mContext, response.body()!!.data.token)
                             finish()
-                        }else{
-                            binding.txtPasswordWarning.text = getString(R.string.not_current_password)
+                        } else {
+                            binding.txtPasswordWarning.text =
+                                getString(R.string.not_current_password)
                         }
                     }
 
                     override fun onFailure(call: Call<BasicResponse>, t: Throwable) {}
                 })
         }
-    }
-
-    override fun setValues() {
-
     }
 }
